42f041cbab382bd5161d6e0bb7bbbceae74a0161
trubeko-sergey <89091179498@yandex.ru>
2023-04-06 18:27:49 +0500

    Update TF

--- Test_WU/main.tf
+++ Test_WU/main.tf
@@ -1,62 +1,78 @@
-// Configure the Yandex.Cloud provider
 terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
+      version = "0.88.0"
     }
   }
 }
+
 provider "yandex" {
   service_account_key_file = "key.json"
   cloud_id                 = "b1g35nhp99idkhi5kja9"     //ID облака
   folder_id                = "b1g36tkee54c3so0d7r7"           //ID Каталога
   zone                     = "ru-central1-a"
 }
-
-//Создание ресурса -> сети
-resource "yandex_vpc_network" "test-lan" {
-  name = "test-wu"
+resource "yandex_vpc_network" "network" {
+  name = "my-network"
 }
-//Подсети
 
-resource "yandex_vpc_subnet" "subnet-wu" {
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "mu-subnet"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["192.168.0.0/24"]
-  network_id     = "${yandex_vpc_network.test-lan.id}"
 }
-
-//Создание инстансов
-resource "yandex_compute_instance" "trubeko" {
-  name        = "test"
-  platform_id = "standard-v1"
-  zone        = "ru-central1-a"
-
-  resources {
-    cores  = 2
-    memory = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = "fd89jk9j9vifp28uprop"
+resource "yandex_compute_instance_group" "mygroup" {
+  name                = "test-ig"
+  folder_id           = "b1g36tkee54c3so0d7r7"
+  service_account_id  = "ajeh4fgcg9na46evbk9e"
+  deletion_protection = true
+  instance_template {
+    platform_id = "standard-v1"
+    resources {
+      memory = 2
+      cores  = 2
+    }
+    boot_disk {
+      mode = "READ_WRITE"
+      initialize_params {
+        image_id = "fd89jk9j9vifp28uprop"
+        //size     = 4
+      }
+    }
+    network_interface {
+      network_id = "${yandex_vpc_network.network.id}"
+      subnet_ids = ["${yandex_vpc_subnet.subnet.id}"]
+    }
+    labels = {
+      label1 = "label1-value"
+      label2 = "label2-value"
+    }
+    metadata = {
+      ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+    }
+    network_settings {
+      type = "STANDARD"
     }
   }
 
-  network_interface {
-    subnet_id = "${yandex_vpc_subnet.subnet-wu.id}"
+  scale_policy {
+    fixed_scale {
+      size = 10
+    }
   }
-  
-  metadata = {
-    ssh-keys = "ubuntu:${file("/root/.ssh/id_rsa.pub")}"
-    //user-data = "${file("init.sh")}" //Согласно документации Яндек.Cloud добавляем скрипт для выполнения постинсталл опеараций
+
+  allocation_policy {
+    zones = ["ru-central1-a"]
   }
+
   deploy_policy {
-    max_unavailable = 2
-    max_creating = 2
-    max_expansion = 2
-    max_deleting = 2
+    max_unavailable = 0
+    max_creating    = 10
+    max_expansion   = 2
+    max_deleting    = 0
   }
 }
-// Create a new instance
-//resource "yandex_compute_instance" "default" {
-//  
-//}
+
+
